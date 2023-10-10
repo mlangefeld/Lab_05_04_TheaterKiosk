77795d84aaad7b5e421dc6d399b3a989e72f039e
mlangefeld <langefmn@mail.uc.edu>
2023-10-10 18:37:15 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int age = 0;
+
+        System.out.print("Enter your age: ");
+
+        if (in.hasNextInt()) {
+            age = in.nextInt();
+            in.nextLine();
+            if (age >= 21)
+            {
+                System.out.println("Over 21 confirmed, here is your wristband");
+            }
+
+        }
+    }
+}
